d17700d5f4a23601c1680c28a92a7f85a8233e03
Ricardo-epse <ricardoepse@gmail.com>
2023-05-08 22:35:56 -0600

    db-failover

---
+++ prueba-dragon-ball-failover/src/main/java/com/example/dragonball/PruebaDragonBallFailoverApplication.java
@@ -0,0 +1,13 @@
+package com.example.dragonball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaDragonBallFailoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaDragonBallFailoverApplication.class, args);
+	}
+
+}
